9f5f2aecad7be0e77c6373aeb91931ae33861d6d
desc0n <desconbk@yandex.ru>
2017-06-13 21:58:19 +1000

    Добавляю API Uniqom

--- application/classes/Model/API.php
+++ application/classes/Model/API.php
@@ -111,7 +111,7 @@ class Model_API extends Kohana_Model
     /**
      * @param string $supplier
      * @param string $article
-     * @return array
+     * @return array|string
      */
     private function getQueryString($supplier, $article)
     {
@@ -126,6 +126,10 @@ class Model_API extends Kohana_Model
                 ];
             case 'rossko':
                 return $article;
+            case 'uniqom':
+                return [
+                    'term' => $article,
+                ];
         }
 
         return [];
@@ -143,6 +147,8 @@ class Model_API extends Kohana_Model
                return $this->parseMxGroupResponse($response);
             case 'rossko':
                return $this->parseRosskoResponse($response);
+            case 'uniqom':
+               return $this->parseUniqomResponse($response);
         }
 
         return [];
@@ -170,6 +176,36 @@ class Model_API extends Kohana_Model
                     'price' => Arr::get($result, 'discountprice', 0),
                     'quantity' => Arr::get($result, 'count', 0),
                     'vendor_id' => Arr::get($result, 'code')
+                ];
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param $response
+     * @return array
+     */
+    private function parseUniqomResponse($response)
+    {
+//        $response = '{"id": 4572,"header": "Щетка стеклоочистителя зимн. Avantech Snowguard 380мм ( 15&#180;&#180; )","brand_id": 11,"brand": "AVANTECH","article_code": "S-15","clone_key": "","variation_code": "380мм ( 15&#180;&#180; )","category_id": 299,"mult_amount": 1,"deposit_storages": [{"id": 56,"header": "Тухачевского","deposit": 49,"days": 0,"price": 287.28},{"id": 76,"header": "ЦРС","deposit": 50,"days": 0,"price": 287.28},{"id": 667,"header": "СВХ","deposit": 0,"days": 2,"price": 287.28}],"images": [{"img100": "/uploads/items/thumb/100_100_0_20.jpg","img210": "/uploads/items/thumb/210_210_0_20.jpg","img130": "/uploads/items/thumb/130_80_0_20.jpg","big": "/uploads/items/20.jpg"}],"price": 287.28}';
+        $data = [];
+        $responseData = json_decode($response, true);
+
+        if(!empty($responseData['deposit_storages'])) {
+            foreach ($responseData['deposit_storages'] as $result) {
+                if (!isset($data[Arr::get($result, 'header')])) {
+                    $data[Arr::get($result, 'header')] = [];
+                }
+
+                $data[Arr::get($result, 'header')][] = [
+                    'brand' => Arr::get($responseData, 'brand'),
+                    'article' => Arr::get($responseData, 'article_code'),
+                    'name' => Arr::get($responseData, 'header'),
+                    'price' => Arr::get($result, 'price', 0),
+                    'quantity' => Arr::get($result, 'deposit', 0),
+                    'vendor_id' => Arr::get($responseData, 'id')
                 ];
             }
         }
